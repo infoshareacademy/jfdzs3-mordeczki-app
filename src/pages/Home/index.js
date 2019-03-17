600b9cb12197a4eec4c04cdb9f6bed60c0384e5e
Marcin Osak <marcin@avangardo.pl>
2019-03-17 15:46:39 +0100

    finished ver 1 home search

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,63 @@
-import React, { Component } from "react";
+import React from "react";
 import "./style.css";
 
-class Home extends Component {
+class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userData: [
+        { name: "fryzjer", date: 666 },
+        { name: "kosmetyczka", date: 123 },
+        { name: "masaż", date: 544 }
+      ],
+      filteredData: [],
+      isFiltered: false
+    };
+    this.handleKeyPress = this.handleKeyPress.bind(this);
+  }
+
+  handleKeyPress(event) {
+    const { value } = event.target;
+    const update = this.state.userData.filter(userData =>
+      userData.name.includes(value)
+    );
+    this.setState({ filteredData: update, isFiltered: true });
+    if (value === "") {
+      this.setState({ isFiltered: false });
+    }
+  }
+
   render() {
-    return <div>HOME PAGE</div>;
+    console.log("Filtered:", this.state.filteredData.length);
+    console.log("Initial:", this.state.userData.length);
+    console.log("Filtered?:", this.state.isFiltered);
+    let renderData = "";
+    if (this.state.filteredData.length !== 0) {
+      renderData = this.state.filteredData;
+    } else {
+      if (!this.state.isFiltered) {
+        renderData = this.state.userData;
+      } else {
+        renderData = [];
+      }
+    }
+
+    return (
+      <div>
+        <input
+          type="text"
+          id="searchCharacters"
+          onChange={this.handleKeyPress}
+        />
+        <ul>
+          {renderData.map(user => (
+            <li key={user.name}>
+              <span>{user.name}</span>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
   }
 }
 
