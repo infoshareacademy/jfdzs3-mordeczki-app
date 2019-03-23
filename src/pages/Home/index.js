444d807d7fc4715522d448aa717e37efd4ea9ee3
Marcin Osak <marcin@avangardo.pl>
2019-03-23 20:35:16 +0100

    late beta of search engine

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,100 +1,63 @@
 import React from "react";
-import "./style.css";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Icon from './icon';
-import {Form, Input, FormGroup, Container, Label} from 'reactstrap';
-import 'react-dates/initialize';
-import 'react-dates/lib/css/_datepicker.css';
-import {SingleDatePicker} from 'react-dates';
+import "react-dates/initialize";
+import moment from "moment";
+import { SingleDatePicker } from "react-dates";
+import "react-dates/lib/css/_datepicker.css";
+
+const userData = [
+  { name: "fryzjer", date: '03/23/2019' },
+  { name: "fryzjer2", date:  '03/24/2019' },
+  { name: "fryzjer3", date:  '03/22/2019' },
+  { name: "kosmetyczka", date:  '03/23/2019' },
+  { name: "masaż", date:  '03/23/2019' }
+];
 
 class Home extends React.Component {
-  constructor(props) {
-    super(props);
+  state = {
+    query: "",
+    queryDate: "",
+    focused: false,
+    date: moment()
+  };
 
-    this.userData = [
-      { name: "fryzjer", date: 666 },
-      { name: "kosmetyczka", date: 123 },
-      { name: "masaż", date: 544 }
-    ]
-
-    this.state = {
-      userData : this.userData,
-      filteredData: [],
-      isFiltered: false,
-      date: null,
-      focused: null
-    };
-    this.handleKeyPress = this.handleKeyPress.bind(this);
-  }
-
-  handleKeyPress(event) {
-    const { value } = event.target;
-    const update = this.state.userData.filter(userData =>
-      userData.name.includes(value)
-    );
-    this.setState({ filteredData: update, isFiltered: true });
-    if (value === "") {
-      this.setState({ isFiltered: false });
-    }
-  }
+  handleInputChange = e => {
+    this.setState({
+      query: e.target.value
+    });
+  };
 
   render() {
-    let renderData = "";
-    if (this.state.filteredData.length !== 0) {
-      renderData = this.state.filteredData;
-    } else {
-      if (!this.state.isFiltered) {
-        renderData = this.state.userData;
-      } else {
-        renderData = [];
-      }
-    }
+    const filteredData = userData.filter(
+      user =>
+        user.name.includes(this.state.query) &&
+        user.date.includes(this.state.date.format("L").toString())
+    );
+
+    console.log(this.state.date.format("L").toString());
+    console.log(userData[0].date);
 
     return (
-      <div>
-        <input
-          type="text"
-          id="searchCharacters"
-          onChange={this.handleKeyPress} />
-          <Form>
-            <FormGroup>
-            <SingleDatePicker
-                          // showClearDate={true}
-                          customInputIcon={
-                            <svg className="icon icon-small">
-                              <Icon
-                                icon="ICON_CALENDER"
-                                className="icon icon-large"
-                              />
-                            </svg>
-                          }
-                          inputIconPosition="after"
-                          small={true}
-                          block={false}
-                          numberOfMonths={1}
-                          date={this.state.date}
-                          onDateChange={date => this.handleDateChange(date)}
-                          focused={this.state.focused}
-                          onFocusChange={({ focused }) =>
-                            this.setState({ focused })
-                          }
-                          openDirection="up"
-                          hideKeyboardShortcutsPanel={true}
-                        />
-            </FormGroup>
-          </Form>
+      <div className="App">
+        <input onChange={this.handleInputChange} />
+        <SingleDatePicker
+          numberOfMonths={1}
+          onDateChange={date => this.setState({ date })}
+          onFocusChange={({ focused }) => this.setState({ focused })}
+          focused={this.state.focused}
+          date={this.state.date}
+        />
 
         <ul>
-          {renderData.map(user => (
+          {filteredData.map(user => (
             <li key={user.name}>
               <span>{user.name}</span>
             </li>
           ))}
+          {null}
+          {filteredData.length === 0 && <div>Nie znaleziono rekordów!</div>}
         </ul>
       </div>
     );
   }
 }
-
 export default Home;
- 
